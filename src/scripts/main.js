fe9b16576449b8ef07ba9813633f97588b46f4e5
Stasik18 <rosgard000@gmail.com>
2026-09-18 13:34:49 +0300

    fix: resolve build paths

---
+++ src/scripts/main.js
@@ -0,0 +1,2 @@
+import '@/assets/styles/global.css'
+import '@/assets/styles/home.css'
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'node:url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  base: '/rsschool-landing-page/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
